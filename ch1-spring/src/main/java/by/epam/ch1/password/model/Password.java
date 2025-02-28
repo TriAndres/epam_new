b3a320800feb8f6e252659dfa9f283e528b12269
Andrei <An.S-1@yandex.ru>
2025-02-28 18:53:55 +0300

    ch1_8

--- ch1-spring/src/main/java/by/epam/ch1/password/model/Password.java
+++ ch1-spring/src/main/java/by/epam/ch1/password/model/Password.java
@@ -1,4 +1,27 @@
 package by.epam.ch1.password.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Password {
+    private Long id;
+    @NotNull
+    @NotBlank
+    @Size(min = 5, message = "login min = 5")
+    private String login;
+    @NotNull
+    @NotBlank
+    @Size(min = 5, message = "password min = 5")
+    private String password;
+    private Boolean registration;
+    private String name;
+    private String localDateTime;
+    private Integer interval;
 }
--- ch1-spring/src/main/java/by/epam/ch1/password/repository/PasswordRepository.java
+++ ch1-spring/src/main/java/by/epam/ch1/password/repository/PasswordRepository.java
@@ -1,4 +1,18 @@
 package by.epam.ch1.password.repository;
 
-public class PasswordRepository {
+import by.epam.ch1.password.model.Password;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface PasswordRepository {
+    Collection<Password> findAll();
+
+    Password save(Password password);
+
+    Optional<Password> findById(long id);
+
+    void deleteById(long id);
+
+    void deleteAll();
 }
