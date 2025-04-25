2bc58cfe52977e9cf652aa1f898f287798716b09
Andrei <An.S-1@yandex.ru>
2025-04-25 16:38:48 +0300

    ch2_16

--- ch2-console/src/main/java/by/epam/number/menu/TaskAServiceMenu.java
+++ ch2-console/src/main/java/by/epam/number/menu/TaskAServiceMenu.java
@@ -12,6 +12,11 @@ public class TaskAServiceMenu extends Menu {
                 "\n\t2 - 2 задача." +
                 "\n\t3 - 3 задача." +
                 "\n\t4 - 4 задача." +
+                "\n\t5 - 5 задача." +
+                "\n\t6 - 6 задача." +
+                "\n\t7 - 7 задача." +
+                "\n\t8 - 8 задача." +
+                "\n\t9 - 9 задача." +
                 "\n\t0 - выход из меню"
         );
         return getString();
@@ -31,6 +36,21 @@ public class TaskAServiceMenu extends Menu {
                 break;
             case "4":
                 taskAService().taskA4();
+                break;
+            case "5":
+                taskAService().taskA5();
+                break;
+            case "6":
+                taskAService().taskA6();
+                break;
+            case "7":
+                taskAService().taskA7();
+                break;
+            case "8":
+                taskAService().taskA8();
+                break;
+            case "9":
+                taskAService().taskA9();
                 break;
             case "0":
                 System.out.println("Выход из меню.");
